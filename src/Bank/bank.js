11bf59fb670d3dcd2bb79ecb85416c5c06eb3a54
princy1612 <70192372+princy1612@users.noreply.github.com>
2020-12-09 01:35:00 -0800

    Add files via upload

---
+++ src/Bank/bank.js
@@ -0,0 +1,459 @@
+//Business Loan Application
+function fullnameTest(fullname)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(fullname);  
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function addressTest(address)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(address);  
+}
+function branchTest(branch)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(branch);  
+}
+function businessnameTest(business)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(business);  
+}
+function amountTest(amount)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(amount);  
+}
+function loantenureTest(loan)
+{
+    let regex = /^[0-9]{1,2}$/;
+    return regex.test(loan);  
+}
+//Create Account
+function firstnameTest(firstname)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(firstname);  
+}
+function lastnameTest(lastname)
+{
+    let regex = /^[a-zA-Z+ .]{2,30}$/;
+    return regex.test(lastname);  
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function emailTest(email) 
+{
+    let regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.com]$/;
+    return regex.test(email);
+}
+function addressTest(address)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(address);  
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+function confirmpasswordTest(confirmpassword) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(confirmpassword);
+}
+//Forgot Password
+function customeridTest(customer)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(customer);  
+}
+function emailTest(email) 
+{
+    let regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.com]$/;
+    return regex.test(email);
+}
+//Home Loan
+function nameTest(name)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(name);  
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function addressTest(address)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(address);  
+}
+function branchTest(branch)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(branch);  
+}
+function amountTest(amount)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(amount);  
+}
+function loantenureTest(loan)
+{
+    let regex = /^[0-9]{1,2}$/;
+    return regex.test(loan);  
+}
+//Apply for Loan on Credit Card   
+function nameTest(name)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(name);  
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function cardtest(card) 
+{
+    let regex = /^[0-9]{16}$/;
+    return regex.test(card);
+}
+function pinTest(password) 
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(password);
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+//Account Login   
+function customeridTest(customer)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(customer);  
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+function pinTest(password) 
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(password);
+}
+//To check Transaction Details  
+function customeridTest(customer)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(customer);  
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+function pinTest(password) 
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(password);
+}
+//Net Banking
+function customeridTest(customer)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(customer);  
+}
+function validTest(valid) 
+{
+    let regex = /^[0-9/]{1,5}$/;
+    return regex.test(valid);
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+function pinTest(password) 
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(password);
+}
+//Personal Loan Application form
+function fullnameTest(fullname)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(fullname);  
+}
+function emailTest(email) 
+{
+    let regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.com]$/;
+    return regex.test(email);
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function accountnotest(account) 
+{
+    let regex = /^[0-9]{16}$/;
+    return regex.test(account);
+}
+function ifscTest(ifsc)
+{
+    let regex = /^[a-zA-Z0-9]{1,12}$/;
+    return regex.test(ifsc);  
+}
+function branchTest(branch)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(branch);  
+}
+//Statement on E-mail
+function customeridTest(customer)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(customer);  
+}
+function emailTest(email) 
+{
+    let regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.com]$/;
+    return regex.test(email);
+}
+function passwordTest(password) 
+{
+    let regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return regex.test(password);
+}
+function pinTest(password) 
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(password);
+}
+describe('Testing the Business Loan Application', ()=>{
+    it("Check for Full Name", ()=>{
+        const res = fullnameTest("Harsha");
+		expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485910");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Address", ()=>{
+        const res = addressTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Branch", ()=>{
+        const res = branchTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Business Name", ()=>{
+        const res = businessnameTest("New Company");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Amount", ()=>{
+        const res = amountTest("100000");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Loan Tenure", ()=>{
+        const res = loantenureTest("5");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Create Account', ()=>{
+    it("Check for First Name", ()=>{
+        const res = firstnameTest("Harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Last Name", ()=>{
+        const res = lastnameTest("C.A");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485901");
+        expect(res).toBeTruthy();
+    })
+    it("Check for E-mail Address", ()=>{
+        const res = emailTest("harshaca@gmail.com");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Address", ()=>{
+        const res = addressTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha@123");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Confirm Password", ()=>{
+        const res = confirmpasswordTest("harsha@123");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Forgot Password', ()=>{
+    it("Check for Customer ID", ()=>{
+        const res = customeridTest("2634");
+        expect(res).toBeTruthy();
+    })
+    it("Check for E-mail Address", ()=>{
+        const res = emailTest("harshaca@gmail.com");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Home Loan', ()=>{
+    it("Check for Name", ()=>{
+        const res = nameTest("Harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485901");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Address", ()=>{
+        const res = addressTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Branch", ()=>{
+        const res = branchTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Amount", ()=>{
+        const res = amountTest("100000");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Loan Tenure", ()=>{
+        const res = loantenureTest("5");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Apply for Loan on Credit Card', ()=>{
+    it("Check for Name", ()=>{
+        const res = nameTest("Harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485901");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Credit Card No.", ()=>{
+        const res = cardtest("9874321685216431");
+        expect(res).toBeTruthy();
+    })
+    it("Check for PIN", ()=>{
+        const res = pinTest("123456");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Account Login', ()=>{
+    it("Check for Customer ID", ()=>{
+        const res = customeridTest("2634");
+        expect(res).toBeTruthy();
+    })
+    it("Check for PIN", ()=>{
+        const res = pinTest("123456");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the To check Transaction Details', ()=>{
+    it("Check for Customer ID", ()=>{
+        const res = customeridTest("2634");
+        expect(res).toBeTruthy();
+    })
+    it("Check for PIN", ()=>{
+        const res = pinTest("123456");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Net Banking', ()=>{
+    it("Check for Customer ID", ()=>{
+        const res = customeridTest("2634");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Valid Through", ()=>{
+        const res = validTest("06/25");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for PIN", ()=>{
+        const res = pinTest("123456");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Personal Loan Application form', ()=>{
+    it("Check for Full Name", ()=>{
+        const res = fullnameTest("Harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for E-mail Address", ()=>{
+        const res = emailTest("harshaca@gmail.com");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485910");
+        expect(res).toBeTruthy();
+    })
+   
+    it("Check for IFSC", ()=>{
+        const res = ifscTest("SHSBK000011");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Branch", ()=>{
+        const res = branchTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+});
+
+describe('Testing the Statement on E-mail', ()=>{
+    it("Check for Customer ID", ()=>{
+        const res = customeridTest("2634");
+        expect(res).toBeTruthy();
+    })
+    it("Check for E-mail Address", ()=>{
+        const res = emailTest("harshaca@gmail.com");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Password", ()=>{
+        const res = passwordTest("harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for PIN", ()=>{
+        const res = pinTest("123456");
+        expect(res).toBeTruthy();
+    })
+});   
